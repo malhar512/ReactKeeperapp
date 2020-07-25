f259de074116004703595ca8e0d9eb0713ad98bc
Malhar Surangalikar <malhar10000@gmail.com>
2020-07-25 12:43:56 +0530

    Updates

---
+++ precache-manifest.e62bb2967acf1d834344ede6aac6390d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "582c4d60182f94f499eebc72dfeb230a",
+    "url": "/ReactKeeperapp/index.html"
+  },
+  {
+    "revision": "dc30940aefa8ead3ab25",
+    "url": "/ReactKeeperapp/static/js/2.722bd8fe.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ReactKeeperapp/static/js/2.722bd8fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c73c2dd87eb370cd439a",
+    "url": "/ReactKeeperapp/static/js/main.24ef8a95.chunk.js"
+  },
+  {
+    "revision": "3c3647e03791778fdbe6",
+    "url": "/ReactKeeperapp/static/js/runtime-main.ff3b1969.js"
+  }
+]);
